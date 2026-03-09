53dcf1f012a94d2ae58313db6fef842f8a532f03
Kuba Ptak <kuba.ptak05@gmail.com>
2026-03-09 13:42:29 +0100

    configured docker container

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -135,7 +135,7 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(minutes=60),
     'ROTATE_REFRESH_TOKENS': True,
     'AUTH_HEADER_TYPES': ('Bearer',),
